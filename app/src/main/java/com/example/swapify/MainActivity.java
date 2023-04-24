9ba6f506a69bdfb78e129a180b8e59170ae5a359
DESKTOP-CVHSL6Q\stoic <elias-valeriu.stoica@s.unibuc.ro>
2023-04-24 21:32:57 +0300

    first commit

--- app/src/main/java/com/example/swapify/MainActivity.java
+++ app/src/main/java/com/example/swapify/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.swapify;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 

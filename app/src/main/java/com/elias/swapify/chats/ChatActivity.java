156230a0d3d01dee092ff5f7828631169343de20
Stoica Elias <stoicaelias2003@outlook.com>
2024-04-08 16:17:34 +0300

    changed chat from listview to recyclerview and added listener for opening keyboard

--- app/src/main/java/com/elias/swapify/chats/ChatActivity.java
+++ app/src/main/java/com/elias/swapify/chats/ChatActivity.java
@@ -6,25 +6,23 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.elias.swapify.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -32,11 +30,9 @@ public class ChatActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private FirebaseUser currentUser;
     private FirebaseFirestore firestoreDB;
-    private CollectionReference chatMessagesCollection;
-    private ListView listViewChatMessages;
+    private RecyclerView recyclerViewChatMessages;
     private EditText editTextMessageInput;
-    private ImageButton buttonSendMessage;
-    private ImageButton backButton;
+    private ImageButton buttonSendMessage, backButton;
     private MessageAdapter messageAdapter;
     private List<MessageModel> messages;
     private TextView otherPersonNameTextView;
@@ -46,166 +42,123 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        getWindow().setBackgroundDrawableResource(R.drawable.background);
 
         firebaseAuth = FirebaseAuth.getInstance();
         firestoreDB = FirebaseFirestore.getInstance();
         currentUser = firebaseAuth.getCurrentUser();
 
-        chatMessagesCollection = firestoreDB.collection("MESSAGES");
+        initViews();
+        setupRecyclerView();
+        setupListeners();
 
-        listViewChatMessages = findViewById(R.id.listViewChatMessages);
+        otherPersonNameTextView.setText(getIntent().getStringExtra("otherPersonName"));
+        loadAvatar();
+        loadMessages();
+
+        final View activityRootView = findViewById(R.id.chatActivityLayout);
+        activityRootView.getViewTreeObserver().addOnGlobalLayoutListener(() -> {
+            int heightDiff = activityRootView.getRootView().getHeight() - activityRootView.getHeight();
+            // If more than 100 pixels, its probably a keyboard...
+            if (heightDiff > dpToPx(100)) { // 100dp threshold is considered as keyboard.
+                if (messageAdapter.getItemCount() > 0) {
+                    recyclerViewChatMessages.smoothScrollToPosition(messageAdapter.getItemCount() - 1);
+                }
+            }
+        });
+    }
+
+    private int dpToPx(int dp) {
+        return (int) (dp * getResources().getDisplayMetrics().density);
+    }
+
+    private void initViews() {
+        recyclerViewChatMessages = findViewById(R.id.recyclerViewChatMessages);
         editTextMessageInput = findViewById(R.id.editTextMessageInput);
         buttonSendMessage = findViewById(R.id.buttonSendMessage);
         backButton = findViewById(R.id.btnBack_chat);
         otherPersonNameTextView = findViewById(R.id.otherPersonNameTextView);
         otherPersonAvatarImageView = findViewById(R.id.otherPersonAvatarImageView);
+    }
 
+    private void setupRecyclerView() {
         messages = new ArrayList<>();
-        messageAdapter = new MessageAdapter(this, R.layout.message_layout_left, messages);
-        listViewChatMessages.setAdapter(messageAdapter);
-        // Move the list view to the bottom
-        listViewChatMessages.setSelection(messageAdapter.getCount() - 1);
+        messageAdapter = new MessageAdapter(this, messages);
+        recyclerViewChatMessages.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewChatMessages.setAdapter(messageAdapter);
+    }
 
-        buttonSendMessage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sendMessage();
+    private void setupListeners() {
+        buttonSendMessage.setOnClickListener(view -> sendMessage());
+
+        backButton.setOnClickListener(v -> finish());
+
+        editTextMessageInput.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) {
+                recyclerViewChatMessages.scrollToPosition(messageAdapter.getItemCount() - 1);
             }
         });
-
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Just finish this activity so it goes back to the previous one
-                finish();
-            }
-        });
-
-        Log.d("ChatActivity", "Receiver name: " + getIntent().getStringExtra("otherPersonName"));
-        otherPersonNameTextView.setText(getIntent().getStringExtra("otherPersonName"));
-
-        loadAvatar();
-        loadMessages();
     }
 
     private void sendMessage() {
         String messageContent = editTextMessageInput.getText().toString().trim();
         if (!messageContent.isEmpty()) {
-            // Create a new message
             MessageModel message = new MessageModel(currentUser.getUid(), getIntent().getStringExtra("receiverId"), messageContent, new Timestamp(new Date()).toDate());
-
-            // Add the message to Firestore
-            chatMessagesCollection.add(message).addOnCompleteListener(new OnCompleteListener() {
-                @Override
-                public void onComplete(@NonNull Task task) {
-                    if (task.isSuccessful()) {
-                        // Clear the message input
-                        editTextMessageInput.setText("");
-                        Toast.makeText(ChatActivity.this, "Message sent!", Toast.LENGTH_SHORT).show();
-                        // Reload the messages
-                        loadMessages();
-                        // Try to add the chat between the two users to the 'CHATS' collection
-                        // This will fail if the chat already exists
-                        addChatToChatsCollection();
-                        // Move the list view to the bottom
-                        listViewChatMessages.setSelection(messageAdapter.getCount() - 1);
-                    } else {
-                        Toast.makeText(ChatActivity.this, "Error sending message!", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
+            firestoreDB.collection("MESSAGES").add(message)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            editTextMessageInput.setText("");
+                            Toast.makeText(ChatActivity.this, "Message sent!", Toast.LENGTH_SHORT).show();
+                            loadMessages(); // Consider optimizing this to not reload all messages
+                        } else {
+                            Toast.makeText(ChatActivity.this, "Error sending message!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
         }
-    }
-
-    private void addChatToChatsCollection() {
-        String user1 = currentUser.getUid();
-        String user2 = getIntent().getStringExtra("receiverId");
-
-        // Create a new chat
-        ChatModel chat = new ChatModel(user1, user2);
-
-        // Add the chat to Firestore but verify if a chat between user1 and user2 exists, or maybe user2 and user1
-        firestoreDB.collection("CHATS")
-                .whereEqualTo("user1", user1)
-                .whereEqualTo("user2", user2)
-                .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful() && task.getResult() != null && task.getResult().isEmpty()) {
-                        firestoreDB.collection("CHATS")
-                                .whereEqualTo("user1", user2)
-                                .whereEqualTo("user2", user1)
-                                .get()
-                                .addOnCompleteListener(task2 -> {
-                                    if (task2.isSuccessful() && task2.getResult() != null && task2.getResult().isEmpty()) {
-                                        firestoreDB.collection("CHATS")
-                                                .add(chat)
-                                                .addOnCompleteListener(task3 -> {
-                                                    if (task3.isSuccessful()) {
-                                                        Toast.makeText(ChatActivity.this, "Chat added to CHATS collection!", Toast.LENGTH_SHORT).show();
-                                                    } else {
-                                                        Toast.makeText(ChatActivity.this, "Error adding chat to CHATS collection!", Toast.LENGTH_SHORT).show();
-                                                    }
-                                                });
-                                    }
-                                });
-                    }
-                });
     }
 
     private void loadMessages() {
         String senderId = currentUser.getUid();
         String receiverId = getIntent().getStringExtra("receiverId");
-        // Create an array of user IDs for the two directions of the chat
-        List<String> chatUserIds = new ArrayList<>();
-        chatUserIds.add(senderId);
-        chatUserIds.add(receiverId);
 
-        // Build a query to get messages sent between the two users in both directions
-        chatMessagesCollection
-                .whereIn("senderId", chatUserIds)
-                .whereIn("receiverId", chatUserIds)
-                .orderBy("datetime")  // You can adjust the ordering as needed
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful() && task.getResult() != null) {
-                            messages.clear();
-                            messages.addAll(task.getResult().toObjects(MessageModel.class));
-                            messageAdapter.notifyDataSetChanged();
-                        }
+        firestoreDB.collection("MESSAGES")
+                .whereIn("senderId", Arrays.asList(senderId, receiverId))
+                .whereIn("receiverId", Arrays.asList(senderId, receiverId))
+                .orderBy("datetime")
+                .addSnapshotListener((queryDocumentSnapshots, e) -> {
+                    if (e != null) {
+                        Log.w("ChatActivity", "Listen failed.", e);
+                        return;
                     }
+
+                    messages.clear();
+                    assert queryDocumentSnapshots != null;
+                    for (DocumentSnapshot doc : queryDocumentSnapshots) {
+                        messages.add(doc.toObject(MessageModel.class));
+                    }
+                    messageAdapter.notifyDataSetChanged();
+                    recyclerViewChatMessages.scrollToPosition(messageAdapter.getItemCount() - 1);
                 });
     }
 
     private void loadAvatar() {
-        // Load the avatar of the other person
         String receiverId = getIntent().getStringExtra("receiverId");
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("USERS")
-                .document(receiverId)
-                .get()
+        FirebaseFirestore.getInstance().collection("USERS").document(receiverId).get()
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful() && task.getResult() != null) {
                         String avatarUrl = task.getResult().getString("profilepicture");
-                        // Check if the avatarUrl is not null and not empty
                         if (avatarUrl != null && !avatarUrl.isEmpty()) {
                             Glide.with(ChatActivity.this)
                                     .load(avatarUrl)
-                                    .placeholder(R.drawable.ic_profile_fulfill) // shown during load
-                                    .error(R.drawable.ic_profile_fulfill) // shown on error
+                                    .placeholder(R.drawable.ic_profile_fulfill)
+                                    .error(R.drawable.ic_profile_fulfill)
                                     .into(otherPersonAvatarImageView);
                         } else {
-                            // Directly load the default avatar if avatarUrl is null or empty
                             otherPersonAvatarImageView.setImageResource(R.drawable.ic_profile_fulfill);
                         }
                     } else {
-                        // In case the task fails or the result is null, directly load the default avatar
                         otherPersonAvatarImageView.setImageResource(R.drawable.ic_profile_fulfill);
                     }
                 });
     }
-
-
-
 }
--- app/src/main/java/com/elias/swapify/chats/MessageAdapter.java
+++ app/src/main/java/com/elias/swapify/chats/MessageAdapter.java
@@ -4,126 +4,106 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.elias.swapify.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.List;
-
 import java.text.SimpleDateFormat;
+import java.util.List;
 import java.util.Locale;
 
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder> {
+    private List<MessageModel> messages;
+    private LayoutInflater inflater;
+    private String currentUserId;
 
-public class MessageAdapter extends ArrayAdapter<MessageModel> {
-    private Context context;
-    private int resource;
+    // Define view types
     private static final int MESSAGE_SENT_BY_ME = 0;
     private static final int MESSAGE_SENT_BY_OTHER = 1;
 
-    public MessageAdapter(Context context, int resource, List<MessageModel> messages) {
-        super(context, resource, messages);
-        this.context = context;
-        this.resource = resource;
+    public MessageAdapter(Context context, List<MessageModel> messages) {
+        this.messages = messages;
+        this.inflater = LayoutInflater.from(context);
+        this.currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    }
+
+    @NonNull
+    @Override
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        if (viewType == MESSAGE_SENT_BY_ME) {
+            view = inflater.inflate(R.layout.message_layout_right, parent, false);
+        } else {
+            view = inflater.inflate(R.layout.message_layout_left, parent, false);
+        }
+        return new MessageViewHolder(view);
     }
 
     @Override
-    public int getViewTypeCount() {
-        // The number of different layouts that will be used in the list
-        return 2;
+    public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
+        MessageModel message = messages.get(position);
+        holder.bind(message);
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
     }
 
     @Override
     public int getItemViewType(int position) {
-        MessageModel message = getItem(position);
-
-        if (message.getSenderId().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
-            // If the current user sent the message, return the sent by me layout
+        MessageModel message = messages.get(position);
+        if (message.getSenderId().equals(currentUserId)) {
             return MESSAGE_SENT_BY_ME;
         } else {
-            // If some other user sent the message, return the sent by other layout
             return MESSAGE_SENT_BY_OTHER;
         }
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder viewHolder;
-        MessageModel message = getItem(position);
-        int viewType = getItemViewType(position);
+    class MessageViewHolder extends RecyclerView.ViewHolder {
+        TextView senderNameTextView, messageContentTextView, messageTimestampTextView;
+        ImageView senderAvatarImageView;
 
-        if (convertView == null) {
-            if (viewType == MESSAGE_SENT_BY_ME) {
-                convertView = LayoutInflater.from(getContext()).inflate(R.layout.message_layout_right, parent, false);
-            } else {
-                convertView = LayoutInflater.from(getContext()).inflate(R.layout.message_layout_left, parent, false);
-            }
-
-            viewHolder = new ViewHolder();
-            viewHolder.senderNameTextView = convertView.findViewById(R.id.senderNameTextView);
-            viewHolder.messageContentTextView = convertView.findViewById(R.id.messageContentTextView);
-            viewHolder.messageTimestampTextView = convertView.findViewById(R.id.messageTimestampTextView);
-            viewHolder.senderAvatarImageView = convertView.findViewById(R.id.smallProfileImage);
-            convertView.setTag(viewHolder);
-        } else {
-            viewHolder = (ViewHolder) convertView.getTag();
+        MessageViewHolder(View itemView) {
+            super(itemView);
+            senderNameTextView = itemView.findViewById(R.id.senderNameTextView);
+            messageContentTextView = itemView.findViewById(R.id.messageContentTextView);
+            messageTimestampTextView = itemView.findViewById(R.id.messageTimestampTextView);
+            senderAvatarImageView = itemView.findViewById(R.id.smallProfileImage);
         }
 
-        if (message != null) {
-            // Fetch the sender's name and avatar
-            fetchSenderAndAvatar(message.getSenderId(), viewHolder.senderNameTextView, viewHolder.senderAvatarImageView);
-            viewHolder.messageContentTextView.setText(message.getContent());
-
-            // Check if the datetime property is not null before converting and displaying it
-            if (message.getDatetime() != null) {
-                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault());
-                viewHolder.messageTimestampTextView.setText(dateFormat.format(message.getDatetime()));
-            } else {
-                viewHolder.messageTimestampTextView.setText("No timestamp available");
-            }
+        void bind(MessageModel message) {
+            messageContentTextView.setText(message.getContent());
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault());
+            messageTimestampTextView.setText(message.getDatetime() != null ? dateFormat.format(message.getDatetime()) : "No timestamp available");
+            fetchSenderAndAvatar(message.getSenderId(), senderNameTextView, senderAvatarImageView);
         }
 
-        return convertView;
-    }
-
-    private void fetchSenderAndAvatar(String userId, final TextView senderNameTextView, final ImageView senderAvatarImageView) {
-        DocumentReference userRef = FirebaseFirestore.getInstance().collection("USERS").document(userId);
-        userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+        private void fetchSenderAndAvatar(String userId, final TextView senderNameTextView, final ImageView senderAvatarImageView) {
+            DocumentReference userRef = FirebaseFirestore.getInstance().collection("USERS").document(userId);
+            userRef.get().addOnCompleteListener(task -> {
                 if (task.isSuccessful() && task.getResult() != null) {
                     DocumentSnapshot document = task.getResult();
                     if (document.exists()) {
                         String userName = document.getString("name");
                         String userAvatarUrl = document.getString("profilepicture");
                         senderNameTextView.setText(userName);
-                        // Now load the avatar using Glide or Picasso
-                        Glide.with(context)
+                        Glide.with(senderAvatarImageView.getContext())
                                 .load(userAvatarUrl)
                                 .placeholder(R.drawable.ic_profile) // default avatar placeholder
                                 .into(senderAvatarImageView);
                     }
                 }
-            }
-        });
-    }
-
-
-    private static class ViewHolder {
-
-        TextView senderNameTextView;
-        TextView messageContentTextView;
-        TextView messageTimestampTextView;
-        ImageView senderAvatarImageView;
+            });
+        }
     }
 }
